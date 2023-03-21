ca592fce748929a9941ccc957da713db60fcfe67
Steven <steven@example.com>
2023-03-20 22:37:42 -0500

    minor cleanup

--- src/prompts/fragment-functions/index.js
+++ src/prompts/fragment-functions/index.js
@@ -1,41 +1,24 @@
-import {
-  asEnum as _asEnum,
-  asIntent as _asIntent,
-  asJSONSchema as _asJSONSchema,
-  asObjectWithSchema as _asObjectWithSchema,
-  asSchemaOrgMessage as _asSchemaOrgMessage,
-  asSchemaOrgType as _asSchemaOrgType,
-} from './output-modifiers.js';
 import _blogPost from './blog-post.js';
 import _generateCollection from './generate-collection.js';
 import _generateList from './generate-list.js';
 import _generateQuestions from './generate-questions.js';
+export {
+  asEnum,
+  asIntent,
+  asJSONSchema,
+  asObjectWithSchema,
+  asSchemaOrgMessage,
+  asSchemaOrgType
+} from './output-modifiers.js';
 import _selectFromThreshold from './select-from-threshold.js';
 import _style from './style.js';
 import _summarize from './summarize.js';
-
-export const asEnum = _asEnum;
-
-export const asIntent = _asIntent;
-
-export const asJSONSchema = _asJSONSchema;
-
-export const asObjectWithSchema = _asObjectWithSchema;
-
-export const asSchemaOrgMessage = _asSchemaOrgMessage;
-
-export const asSchemaOrgType = _asSchemaOrgType;
-
-export const blogPost = _blogPost;
-
-export const generateCollection = _generateCollection;
-
-export const generateList = _generateList;
-
-export const generateQuestions = _generateQuestions;
-
-export const selectFromThreshold = _selectFromThreshold;
-
-export const style = _style;
-
-export const summarize = _summarize;
+export {
+  _blogPost as blogPost,
+  _generateCollection as generateCollection,
+  _generateList as generateList,
+  _generateQuestions as generateQuestions,
+  _selectFromThreshold as selectedFromThreshold,
+  _style as style,
+  _summarize as summarize,
+};
