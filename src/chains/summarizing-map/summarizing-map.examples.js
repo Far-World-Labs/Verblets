cd1ee28fb15a3b849629ab381c3ade76bd1b3f99
Steven <steven@example.com>
2023-04-16 23:10:29 -0500

    Fix example for summarizing map

--- src/chains/summarizing-map/summarizing-map.examples.js
+++ src/chains/summarizing-map/summarizing-map.examples.js
@@ -1,28 +1,45 @@
 import { describe, expect, it, vi } from 'vitest';
 
-import summarizingMap from './index.js';
+import SummarizingMap from './index.js';
 
-const examples = [
-  {
-    inputs: { text: 'test' },
-    want: { result: true }
+const legalText = `Pursuant to the stipulations delineated herein, the parties hereto, designated as Party A (the "Grantor") and Party B (the "Grantee"), do hereby irrevocably and unconditionally covenant to abide by the complex and intricate provisions associated with the lesser-known subject matter of usufructuary rights in the realm of riparian watercourses, specifically encompassing the doctrine of correlative rights and the principle of reasonable use, in accordance with the heretofore undisclosed specifications set forth in Schedule U-1.
+
+In pursuance of the aforesaid stipulations, the Grantee shall obtain a non-exclusive, non-transferable, revocable license, subject to the limitations and conditions imposed by the pertinent jurisdictional authorities, to make equitable utilization of the watercourse, taking into account the natural flow, hydrological cycle, and ecological balance, while ensuring the avoidance of significant harm to other riparians by employing the best practicable means in conformity with the established standards of reasonableness, as delineated in the annexed Exhibit R-1, which sets forth the methodology for the determination of the equitable apportionment of said watercourse, and the allocation of the respective usufructuary rights.`;
+
+const codeText = `
+function _generateKey(seed, length) {
+  let key = '';
+  let curr = seed;
+  for (let i = 0; i < length; i++) {
+    curr = (1664525 * curr + 1013904223) % 4294967296;
+    key += String.fromCharCode(curr % 256);
   }
-];
+  return key;
+}
 
-describe('Summarizing map verblet', () => {
-  examples.forEach((example) => {
-    it(example.inputs.text, async () => {
-      const result = await summarizingMap(example.inputs.text)
+function _xorStrings(a, b) {
+  let res = '';
+  for (let i = 0; i < a.length; i++) {
+    res += String.fromCharCode(a.charCodeAt(i) ^ b.charCodeAt(i));
+  }
+  return res;
+}
 
-      if (example.want.typeOfResult) {
-        expect(typeof result)
-          .toStrictEqual(example.want.typeOfResult);
-      }
+function encodeDecode(input, seed) {
+  let key = _generateKey(seed, input.length);
+  return _xorStrings(input, key);
+}
+`;
 
-      if (example.want.result) {
-        expect(result)
-          .toStrictEqual(example.want.result);
-      }
-    });
+describe('Summarizing map', () => {
+  it('Example', async () => {
+    const map = new SummarizingMap(100);
+
+    map.set({ key: 'a.b.c', value: legalText, weight: 0.2 });
+    map.set({ key: 'a.d', value: codeText, weight: 0.7 });
+
+    // console.log(await map.getAll())
+
+    expect(1).toBe(1);
   });
 });
--- src/chains/test-advice/index.js
+++ src/chains/test-advice/index.js
@@ -1,40 +1,40 @@
 import test from '../test/index.js';
 
-export default async () => {
-   const issues = await test('./src/lib/path-aliases/index.js', 'Run the code with 5 boundary value test cases and report any that fail');
+export default async (path) => {
+   const issues = await test(path, 'Run the code with 5 boundary value test cases and report any that fail');
 
   const issuesSuccess = await test(
-    './src/lib/path-aliases/index.js',
+    path,
     'Identify 5 passing scenarios and significant boundary conditions in this code. Provide minimal input examples for each scenario to demonstrate correctness.',
   );
 
   const issuesFail = await test(
-    './src/lib/path-aliases/index.js',
+    path,
     'Identify 5 failing scenarios and significant boundary conditions in this code. Provide minimal input examples for each scenario to demonstrate the failure. Assume DBC, and don\'t complain when types are specified in jsDoc.',
   );
 
   const issuesDefects = await test(
-    './src/lib/path-aliases/index.js',
+    path,
     'Identify 5 defects in this code. Provide minimal input examples to demonstrate each defect.',
   );
 
   const issuesBestPractices = await test(
-    './src/lib/path-aliases/index.js',
+    path,
     'Suggest 5 best practices improvements for this code.'
   );
 
   const issuesCleanCode = await test(
-    './src/lib/path-aliases/index.js',
+    path,
     'Suggest 5 "clean code" improvements for this code.'
   );
 
   const issuesQuality = await test(
-    './src/lib/path-aliases/index.js',
+    path,
     'Identify 5 specific issues related to code quality, readability, and maintainability.',
   );
 
   const issuesRefactors = await test(
-    './src/lib/path-aliases/index.js',
+    path,
     'Suggest 5 refactors that would most improve the composibility of this code.',
   );
 
