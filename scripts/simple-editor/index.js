8adcf3bce4e5d101db66010e57c5145429d355f7
Steven <steven@example.com>
2023-04-27 01:36:31 -0500

    Linting, runner, and package housekeeping

--- scripts/simple-editor/index.js
+++ scripts/simple-editor/index.js
@@ -1,36 +1,56 @@
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 import edit from '../../src/lib/editor/index.js';
-
 import chatGPT, { getRedis, intent, list } from '../../src/index.js';
 
-const operations = [{
-  name: 'completion',
-  parameters: ['text'],
-  operation: ({ text }) => chatGPT(text),
-}, {
-  name: 'list',
-  parameters: ['description'],
-  operation: ({ description }) => list(description),
-}];
+const requestTimeout = 30000;
 
-const command = await intent({
-  text: (await edit()),
-  operations,
-});
+const argv = yargs(hideBin(process.argv))
+  .option('use-intent', {
+    alias: 'i',
+    default: 'true',
+    type: 'string',
+    description: 'Disable intent parsing and just use chatGPT',
+  })
+  .argv;
+
+const operations = [
+  {
+    name: 'completion',
+    parameters: ['text'],
+    operation: ({ text }) => chatGPT(text, { requestTimeout }),
+  },
+  {
+    name: 'list',
+    parameters: ['description'],
+    operation: ({ description }) => list(description),
+  },
+];
+
+const text = await edit();
+
+console.error(`${text}`);
+
+const command = !['true', '1'].includes(argv.useIntent.toLowerCase())
+  ? { intent: { operation: 'completion' }, parameters: { text } }
+  : await intent({
+      text,
+      operations,
+    });
 
 const operation = operations
-  .find(o => o.name === command.intent.operation)
+  .find((o) => o.name === command.intent.operation)
   ?.operation;
 
 if (operation) {
   const result = await operation(command.parameters);
 
-  console.error(`Command: ${command.intent.displayName}`);
-  Object.entries(command.parameters ?? {})
-    .forEach(entry => {
-      console.error(`  ${entry[0]}: ${entry[1]}`);
-    });
+  console.error(`Command: ${command.intent.name}`);
+  Object.entries(command.parameters ?? {}).forEach((entry) => {
+    console.error(`│ ${entry[0]}: ${entry[1]}`);
+  });
 
-  console.error(`\n${JSON.stringify(result, null, 2)}`);
+  console.error(result);
 }
 
 await (await getRedis()).disconnect();
