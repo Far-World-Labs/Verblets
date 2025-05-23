817f6a729783864dcb3c7edd3a4ebdb553f0bffe
Steven Eberling <flowgrammer@users.noreply.github.com>
2025-05-23 14:00:32 -0500

    refactor: namespace exports

--- src/index.js
+++ src/index.js
@@ -4,30 +4,118 @@ import dotenv from 'dotenv/config';
 
 import chatGPT from './lib/chatgpt/index.js';
 
-export { default as Dismantle } from './chains/dismantle/index.js';
-export { default as list } from './chains/list/index.js';
+// chains are consumed as verblets
+// eslint-disable-next-line import/no-named-as-default
+import anonymize from './chains/anonymize/index.js';
+// eslint-disable-next-line import/no-named-as-default
+import Dismantle from './chains/dismantle/index.js';
+// eslint-disable-next-line import/no-named-as-default
+import list from './chains/list/index.js';
+// eslint-disable-next-line import/no-named-as-default
+import questions from './chains/questions/index.js';
+// eslint-disable-next-line import/no-named-as-default
+import scanJS from './chains/scan-js/index.js';
+// eslint-disable-next-line import/no-named-as-default
+import sort from './chains/sort/index.js';
+// eslint-disable-next-line import/no-named-as-default
+import SummaryMap from './chains/summary-map/index.js';
+// eslint-disable-next-line import/no-named-as-default
+import test from './chains/test/index.js';
+// eslint-disable-next-line import/no-named-as-default
+import testAdvice from './chains/test-advice/index.js';
 
-export { default as questions } from './chains/questions/index.js';
-export { default as scanJS } from './chains/scan-js/index.js';
-export { default as sort } from './chains/sort/index.js';
-export { default as SummaryMap } from './chains/summary-map/index.js';
+import schemas from './json-schemas/index.js';
+import * as common from './constants/common.js';
+import * as messages from './constants/messages.js';
+import * as models from './constants/models.js';
 
-export { default as schemas } from './json-schemas/index.js';
+// exported lib utilities
+// internal-only: anySignal, editor, parseJsParts, pathAliases, pave, TimedAbortController
+import * as promptCache from './lib/prompt-cache/index.js';
+import retry from './lib/retry/index.js';
+import searchBestFirst from './lib/search-best-first/index.js';
+import searchJSFiles from './lib/search-js-files/index.js';
+import shortenText from './lib/shorten-text/index.js';
+import stripNumeric from './lib/strip-numeric/index.js';
+import stripResponse from './lib/strip-response/index.js';
+import toBool from './lib/to-bool/index.js';
+import toEnum from './lib/to-enum/index.js';
+import toNumber from './lib/to-number/index.js';
+import toNumberWithUnits from './lib/to-number-with-units/index.js';
+import transcribe from './lib/transcribe/index.js';
 
-export { default as retry } from './lib/retry/index.js';
-export { default as stripResponse } from './lib/strip-response/index.js';
-export { default as searchJSFiles } from './lib/search-js-files/index.js';
+// prompts
+import * as prompts from './prompts/index.js';
 
-export * as prompts from './prompts/index.js';
+// services
+import * as redis from './services/redis/index.js';
+import modelService from './services/llm-model/index.js';
 
-export { getClient as getRedis } from './services/redis/index.js';
+// verblets
+// eslint-disable-next-line import/no-named-as-default
+import auto from './verblets/auto/index.js';
+// eslint-disable-next-line import/no-named-as-default
+import bool from './verblets/bool/index.js';
+// eslint-disable-next-line import/no-named-as-default
+import enums from './verblets/enum/index.js';
+// eslint-disable-next-line import/no-named-as-default
+import intent from './verblets/intent/index.js';
+// eslint-disable-next-line import/no-named-as-default
+import number from './verblets/number/index.js';
+// eslint-disable-next-line import/no-named-as-default
+import numberWithUnits from './verblets/number-with-units/index.js';
+// eslint-disable-next-line import/no-named-as-default
+import schemaOrg from './verblets/schema-org/index.js';
+// eslint-disable-next-line import/no-named-as-default
+import toObject from './verblets/to-object/index.js';
 
-export { default as auto } from './verblets/auto/index.js';
-export { default as bool } from './verblets/bool/index.js';
-export { default as enums } from './verblets/enum/index.js';
-export { default as intent } from './verblets/intent/index.js';
-export { default as number } from './verblets/number/index.js';
-export { default as schemaOrg } from './verblets/schema-org/index.js';
-export { default as toObject } from './verblets/to-object/index.js';
+export const lib = {
+  chatGPT,
+  promptCache,
+  retry,
+  searchBestFirst,
+  searchJSFiles,
+  shortenText,
+  stripNumeric,
+  stripResponse,
+  toBool,
+  toEnum,
+  toNumber,
+  toNumberWithUnits,
+  transcribe,
+};
+
+export const verblets = {
+  auto,
+  bool,
+  enums,
+  intent,
+  number,
+  numberWithUnits,
+  schemaOrg,
+  toObject,
+  anonymize,
+  Dismantle,
+  list,
+  questions,
+  scanJS,
+  sort,
+  SummaryMap,
+  test,
+  testAdvice,
+};
+
+export const services = {
+  redis,
+  modelService,
+};
+
+export const constants = {
+  common,
+  messages,
+  models,
+};
+
+export { prompts, schemas };
 
 export default chatGPT;
